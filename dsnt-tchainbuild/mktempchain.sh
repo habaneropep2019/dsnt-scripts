169cbd29a803160ea2afaefe5fc97bd5016d973e
Alec Bloss <hopefullifetwo@gmail.com>
2019-01-30 20:24:02 -0600

    Updated licensing

--- dsnt-tchainbuild/mktempchain.sh
+++ dsnt-tchainbuild/mktempchain.sh
@@ -4,6 +4,39 @@
 
 # This is a Dissonant build utility.
 
+# This program is dual-licensed.
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are
+# met:
+#
+#    (1) Redistributions of source code must retain the above copyright
+#    notice, this list of conditions and the following disclaimer. 
+#
+#    (2) Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in
+#    the documentation and/or other materials provided with the
+#    distribution.  
+#    
+#    (3)The name of the author may not be used to
+#    endorse or promote products derived from this software without
+#    specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+# WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+# (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 # This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU General Public License as published by
 #    the Free Software Foundation, either version 3 of the License, or
@@ -16,6 +49,8 @@
 #
 #    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
 
 # This script builds the tempchain
 
@@ -215,6 +250,7 @@ touch $HOME/dsnt-working/$DSNTBUILDID/.status/create_diskimage_mnt
 
 echo "Generating the script needed for the next step, please wait..."
 
+echo "Put untar and copy part here..."
 
 dsp_clear
 
@@ -244,6 +280,31 @@ echo "Please confirm that you have run asrootsetup.sh"
 echo "and that it has completed successfully\?"
 echo "If not, please do so before continuing."
 read -p "Press any key to continue."
+
+##log
+echo "`date +%Y%m%d_%H%M%S%Z` Attempting to mount the diskimage as a normal user" >> $HOME/dsnt-working/$DSNTBUILDID/log/mktempchain.log
+##endlog
+
+echo "Attempting to mount the diskimage, please wait..."
+mount -v $HOME/dsnt-working/$DSNTBUILDID/disk/mnt 2>&1 | tee $HOME/dsnt-working/$DSNTBUILDID/log/mktempchain.log
+echo "Checking for successful mount..."
+
+if [ ! -d "$HOME/dsnt-working/$DSNTBUILDID/disk/mnt/lost+found" ] # <<Change this to a created directory
+	then
+		echo "Diskimage mount as normal user failed."
+		##log
+		echo "`date +%Y%m%d_%H%M%S%Z` Diskimage mount as normal user failed. See log lines above." >> $HOME/dsnt-working/$DSNTBUILDID/log/mktempchain.log
+		##endlog
+		exit 1
+fi
+
+echo "Mount appears to be successful."
+##log
+echo "`date +%Y%m%d_%H%M%S%Z` Mounted diskimage as a normal user successfully" >> $HOME/dsnt-working/$DSNTBUILDID/log/mktempchain.log
+##endlog
+##status
+touch $HOME/dsnt-working/$DSNTBUILDID/.status/mount_diskimage_normal_user_success
+##
 
 
 
